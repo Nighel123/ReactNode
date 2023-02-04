6a90828592a5246497bc8b9ca861e3c2e715b833
Nighel123 <nickel.paulsen@gmail.com>
2023-02-04 20:59:39 +0100

    Now we have a Database

--- server/server.js
+++ server/server.js
@@ -1,5 +1,19 @@
+require('dotenv').config()
+
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
+
+mongoose.set('strictQuery', true);
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true})
+const db = mongoose.connection
+db.on('error', (error) => console.error(error))
+db.on('open', () => console.log('Connected to Database'))
+
+app.use(express.json())
+
+const subscribersRouter = require('./routes/subscribers')
+app.use('/subscribers', subscribersRouter)
 
 app.get("/api",(req,res) => {
 	res.json({ "users":["userOne","userTwo","userThree"] })
